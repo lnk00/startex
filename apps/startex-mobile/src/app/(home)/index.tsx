4d3cc175963d2e062a51e2c100c4e34910bd7674
Damien Dumontet <damien.dumontet@protonmail.com>
2025-01-18 23:36:02 -0300

    remove core

--- apps/startex-mobile/src/app/(home)/index.tsx
+++ apps/startex-mobile/src/app/(home)/index.tsx
@@ -1,10 +1,7 @@
 import { StyleSheet, View, Text } from 'react-native';
 import { Link } from 'expo-router';
-import { startexCore } from '@startex/core';
 
 export const Index = () => {
-  console.log(startexCore);
-
   return (
     <View style={styles.container}>
       <Text>startex</Text>
--- libs/startex-core/README.md
+++
@@ -1,7 +0,0 @@
-# startex-core
-
-This library was generated with [Nx](https://nx.dev).
-
-## Building
-
-Run `nx build startex-core` to build the library.
